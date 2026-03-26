e843520a97a91c18876913f3c85c4df8b4fa8d38
braiseandco <contact@braiseandco-biganos.fr>
2026-03-26 11:16:55 +0100

    Fix caisse SW : n'efface que ses propres caches (caisse-*)

--- caisse/sw.js
+++ caisse/sw.js
@@ -12,7 +12,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      keys.filter(k => k.startsWith('caisse-') && k !== CACHE).map(k => caches.delete(k))
     )).then(() => self.clients.claim())
   );
 });
